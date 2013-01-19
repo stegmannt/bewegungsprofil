885cdd75c9a4199c0751ef7bc3695a421463d441
Tobias Stegmann <stegmannt@gmail.com>
2013-01-19 23:36:30 +0100

    base functionality done

--- bewegungsprofil.rb
+++ bewegungsprofil.rb
@@ -1,14 +1,42 @@
 # encoding: utf-8
 
 require 'exifr'
+require 'googlestaticmap'
+
+coordinates = []
+olddir = Dir.pwd
+images = []
 
 Dir.chdir(ARGV[0])
-Dir.glob("*.{jpg,jpeg,JPG,JPEG}") { |file|
-					image = EXIFR::JPEG.new(file)
+files = Dir.glob("*.{jpg,jpeg,JPG,JPEG}") { |file|
+				   image = EXIFR::JPEG.new(file)
                                   if image.exif? then
+				      images << image
 				      puts "#{file}: Erstellt: #{image.date_time}, Long. #{image.gps.longitude} Lat. #{image.gps.latitude}"
                                   else
 				      puts "#{file}: no exif information found"
                                   end
                                   }
+
+Dir.chdir(olddir)
+
+#sort by creation time
+images.sort! {|x,y| x.date_time <=> y.date_time}
+
+#create the static map
+map = GoogleStaticMap.new(:maptype => "sattelite", :format => "jpg", :width => 1024, :height => 768)
+poly = MapPolygon.new(:color => "red")
+
+i='A'
+images.each { |image|
+                   location = MapLocation.new(:longitude => image.gps.longitude, :latitude => image.gps.latitude)
+                 poly.points << location
+                 map.markers << MapMarker.new(:location => location, :label => i)
+                 i=i.succ
+                 }
+map.paths << poly
+map.get_map("bewegungsprofil.jpg");
+
+puts "Created movement profile: bewegungsprofil.jpg"
+puts "\nUrl: #{map.url}"
 
